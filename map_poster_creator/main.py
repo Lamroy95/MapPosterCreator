58118e315c9efbf6dfad8572707c8093c9b9573f
Lamroy95 <aargh95@gmail.com>
2021-05-09 00:04:29 +0300

    Add CPU architecture type

--- map_poster_creator/main.py
+++ map_poster_creator/main.py
@@ -1,6 +1,6 @@
 from typing import Tuple, List, Dict, Union
 
-import fiona  # noqa For Apple Silicon (M1) CPU
+import fiona  # noqa For Apple Silicon (M1) ARM-based CPU
 # it needs! Else Exception!
 from geopandas import GeoDataFrame
 from shapely.geometry import Polygon
